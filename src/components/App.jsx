64fa7ac1d7f1f90eb1138689f8e751160224b7a7
rooddeeo <rooddeeo@gmail.com>
2023-11-17 23:04:09 +0200

    React Home Work 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactList from './ContactList/ContactList.jsx';
+import ContactForm from './ContactForm/ContactForm.jsx';
+import { nanoid } from 'nanoid';
+import Filter from './Filter/Filter.jsx';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+  };
+
+  addContact = newContact => {
+    const isExist = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+    if (isExist) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    this.setState(prev => ({
+      contacts: [
+        ...prev.contacts,
+        {
+          ...newContact,
+          id: nanoid(),
+        },
+      ],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  componentDidMount() {
+    const contactsBase = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contactsBase) || [];
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const lowerCase = this.state.filter.toLowerCase();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerCase)
+    );
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} changeFilter={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import css from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addContact(this.state);
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className={css.contactBlock}>
+          <label className={css.contactFormLable}>Name</label>
+          <input
+            className={css.contactInputForm}
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </div>
+
+        <div className={css.contactBlock}>
+          <label className={css.contactFormLable}>Number</label>
+          <input
+            className={css.contactInputForm}
+            type="tel"
+            name="number"
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </div>
+
+        <button className={css.contactBtn} type="submit">
+          Add Contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
