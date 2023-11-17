9d62c7cc61d2c14548bb31d314c4f62d4783e21c
rooddeeo <rooddeeo@gmail.com>
2023-11-18 00:42:02 +0200

    React Home Work 3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -29,6 +29,7 @@ class ContactForm extends Component {
             className={css.contactInputForm}
             type="text"
             name="name"
+            value={this.state.name}
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -42,6 +43,7 @@ class ContactForm extends Component {
             className={css.contactInputForm}
             type="tel"
             name="number"
+            value={this.state.number}
             onChange={this.handleChange}
             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
